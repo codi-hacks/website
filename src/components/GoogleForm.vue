9efd64f8c7d6710c18b1f0aba58cf7dc7748e05d
Trevor Corcoran <supershadowplay@gmail.com>
2020-11-06 14:01:14 -0500

    GoogleForm.vue tabs replaced with spaces

--- src/components/GoogleForm.vue
+++ src/components/GoogleForm.vue
@@ -1,20 +1,20 @@
 <template>
-    <p>If you are interested in joining, please fill out your contact info in the form below and our club president will get back to you.</p>
-    <p>Programmers of all skill levels welcome!</p>
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd1__tmuMPAw5j3uKujuDw1ibA2vSBYvu3Ww50eGQJIj6EejA/viewform?embedded=true"
-    width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Sign-up form is loading...</iframe> 
+  <p>If you are interested in joining, please fill out your contact info in the form below and our club president will get back to you.</p>
+  <p>Programmers of <b>all</b> skill levels welcome!</p>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd1__tmuMPAw5j3uKujuDw1ibA2vSBYvu3Ww50eGQJIj6EejA/viewform?embedded=true"
+  width="100%" height="1050" frameborder="0" marginheight="0" marginwidth="0">Sign-up form is loading...</iframe> 
 </template>
 
 <script>
 export default {
-    name: "GoogleForm"
+  name: "GoogleForm"
 }
 </script>
 
 <style scoped>
 iframe {
-    max-width: 75%;
-    min-width: 200px;
-    max-height: auto;
+  max-width: 90%;
+  min-width: 200px;
+  max-height: auto;
 }
 </style>

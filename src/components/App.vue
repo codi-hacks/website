b9974cf06292c2e8392b51a5c13572c3eafffe54
Trevor Corcoran <supershadowplay@gmail.com>
2022-09-10 20:39:33 -0400

    Removed GoogleForm component.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,20 +1,17 @@
 <template>
   <Header :dark-mode="darkMode" />
-  <GoogleForm/>
   <Footer :darkMode="darkMode" @update:darkMode="toggleDarkMode" />
 </template>
 
 <script>
 import Header from './Header'
 import Footer from './Footer'
-import GoogleForm from './GoogleForm'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer,
-    GoogleForm
+    Footer
   },
   data() {
     return {
